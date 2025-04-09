752d39b1f53a7f3b4972b1ebbab406267be7d78e
ivanochoa229 <ivanochoa229@gmail.com>
2025-04-09 14:00:18 -0300

    creando clases auxiliares para delegar trabajo del controller

--- src/main/java/org/simulacion/presentation/controller/MiddleSquareController.java
+++ src/main/java/org/simulacion/presentation/controller/MiddleSquareController.java
@@ -4,16 +4,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import org.simulacion.configuration.AppConfig;
-import org.simulacion.exception.InvalidInputException;
 import org.simulacion.generator.MiddleSquare;
 import org.simulacion.presentation.dto.MiddleSquareRequest;
 import org.simulacion.service.MiddleSquareService;
+import utils.InputCleaner;
 import utils.Path;
+import utils.ViewUtils;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.UnaryOperator;
-import java.util.stream.Collectors;
 
 public class MiddleSquareController {
 
@@ -42,10 +40,10 @@ public class MiddleSquareController {
                                                                     Integer.parseInt(txtDigits.getText()),
                                                                     Integer.parseInt(txtQuantity.getText()));
             List<Double> numbers = service.generateNumbers(request);
-            txtFieldNumbers.setText(formatNumbers(numbers));
-            clearInputFields();
-        } catch (InvalidInputException | NumberFormatException e) {
-            showErrorAlert("Error", e.getMessage());
+            txtFieldNumbers.setText(ViewUtils.formatNumbers(numbers));
+            InputCleaner.clearInputFields(txtSead, txtDigits, txtQuantity);
+        } catch (Exception e) {
+            ViewUtils.showErrorAlert("Error", e.getMessage());
         }
     }
 
@@ -61,45 +59,11 @@ public class MiddleSquareController {
 
     @FXML
     public void initialize() {
-        setupIntegerTextField(txtSead);
-        setupIntegerTextField(txtDigits);
-        setupIntegerTextField(txtQuantity);
+        ViewUtils.setupIntegerTextField(txtSead);
+        ViewUtils.setupIntegerTextField(txtDigits);
+        ViewUtils.setupIntegerTextField(txtQuantity);
 
-        txtSead.setOnMouseClicked(e -> txtFieldNumbers.clear());
-        txtDigits.setOnMouseClicked(e -> txtFieldNumbers.clear());
-        txtQuantity.setOnMouseClicked(e -> txtFieldNumbers.clear());
+        InputCleaner.clearTextAreaOnFocus(txtFieldNumbers, txtSead, txtDigits, txtQuantity);
     }
 
-    private void setupIntegerTextField(TextField textField) {
-        UnaryOperator<TextFormatter.Change> filter = change -> {
-            String newText = change.getControlNewText();
-            if (newText.matches("\\d*")) {
-                return change;
-            }
-            return null;
-        };
-        textField.setTextFormatter(new TextFormatter<>(filter));
-    }
-
-
-    private String formatNumbers(List<Double> numbers) {
-        AtomicInteger counter = new AtomicInteger();
-        return numbers.stream()
-                .map(n -> {
-                    int count = counter.getAndIncrement();
-                    return n.toString() + ((count + 1) % 5 == 0 && count != numbers.size() - 1 ? "\n" : "  ");
-                })
-                .collect(Collectors.joining()).trim();
-    }
-
-    private void clearInputFields() {
-        txtSead.clear();
-        txtDigits.clear();
-        txtQuantity.clear();
-    }
-
-    private void showErrorAlert(String title, String message) {
-        new Alert(Alert.AlertType.ERROR, message, ButtonType.OK)
-                .showAndWait();
-    }
 }
---
+++ src/main/java/utils/ViewUtils.java
@@ -0,0 +1,37 @@
+package utils;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+
+public final class ViewUtils {
+
+    public static void setupIntegerTextField(TextField textField) {
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newText = change.getControlNewText();
+            return newText.matches("\\d*") ? change : null;
+        };
+        textField.setTextFormatter(new TextFormatter<>(filter));
+    }
+
+    public static String formatNumbers(List<Double> numbers) {
+        AtomicInteger counter = new AtomicInteger();
+        return numbers.stream()
+                .map(n -> {
+                    int count = counter.getAndIncrement();
+                    return n.toString() + ((count + 1) % 5 == 0 && count != numbers.size() - 1 ? "\n" : "  ");
+                })
+                .collect(Collectors.joining()).trim();
+    }
+
+    public static void showErrorAlert(String title, String message) {
+        new Alert(Alert.AlertType.ERROR, message, ButtonType.OK).showAndWait();
+    }
+}

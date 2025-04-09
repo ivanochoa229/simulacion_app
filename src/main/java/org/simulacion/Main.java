72e0085cb87da54be95ee10e47761dfed85e5fd3
mauroarms <mauro.armas14@gmail.com>
2025-04-08 21:59:36 -0300

    Se creo el metodo congruencial multiplicativo (también se comentaron las lineas del metodo anterior para probar el actual)

--- src/main/java/org/simulacion/Main.java
+++ src/main/java/org/simulacion/Main.java
@@ -1,13 +1,22 @@
 package org.simulacion;
 
 import org.simulacion.generator.MiddleSquare;
+import org.simulacion.generator.MultCongruential;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        MiddleSquare middleSquare = new MiddleSquare();
-        List<Double> numbers = middleSquare.getNumbers(123, 4, 5);
+
+        /* METODO PARTE CENTRAL DEL CUADRADO */
+        /* MiddleSquare middleSquare = new MiddleSquare();
+         List<Double> numbers = middleSquare.getNumbers(123, 4, 5);
+        numbers.forEach(System.out::println); */
+
+        /* METODO CONGRUENCIAL MULTIPLICATIVO */
+        MultCongruential multCongruential = new MultCongruential();
+        List<Double> numbers = multCongruential.getNumbers(1317, 5631, 547, 5);
         numbers.forEach(System.out::println);
+
     }
 }

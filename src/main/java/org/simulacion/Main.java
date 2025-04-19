96b657f98e869d7b542f04994518fdd4fa15e7e5
Julieta Sleiman <Julietasleiman@gmail.com>
2025-04-19 13:11:13 -0300

    Se agrego el metodo congruencial mixto

--- src/main/java/org/simulacion/Main.java
+++ src/main/java/org/simulacion/Main.java
@@ -4,6 +4,7 @@ import org.simulacion.generator.Lehmer;
 import org.simulacion.generator.MiddleSquare;
 import org.simulacion.generator.MultCongruential;
 import org.simulacion.generator.AdditCongruential;
+import org.simulacion.generator.MixedCongruential;
 
 import java.util.List;
 
@@ -23,11 +24,19 @@ public class Main {
         /* METODO CONGRUENCIAL ADITIVO */
         AdditCongruential additCongruential = new AdditCongruential();
         List<Double> numbers = additCongruential.getNumbers(3, 5147, 3);
+        System.out.println("Números generados por el Metodo Congruencial Aditivo:\t");
         numbers.forEach(System.out::println);
 
         /* METODO LEHMER */
         Lehmer lehmer = new Lehmer();
         List<Double> numbersLehmer = lehmer.getNumbers(73, 35451, 8);
+        System.out.println("Numeros generados por el Metodo Lehmer:\t");
         numbersLehmer.forEach(System.out::println);
+        
+        /*METODO CONGRUENCIAL MIXTO*/
+       MixedCongruential mc = new MixedCongruential(5, 1, 16, 7);
+       System.out.println("Números generados por el Método Congruencial Mixto:\t");
+       mc.generate(10).forEach(System.out::println);
+
     }
 }

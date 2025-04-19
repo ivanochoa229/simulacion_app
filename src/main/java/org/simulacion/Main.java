57e0108676a8d8e37cb5254e00a23a7e1d8f9785
Alejandro Ivan Ochoa <122215963+ivanochoa229@users.noreply.github.com>
2025-04-19 13:30:44 -0300

    Update Main.java
    se eliminaron los metodos del main

--- src/main/java/org/simulacion/Main.java
+++ src/main/java/org/simulacion/Main.java
@@ -11,32 +11,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        /* METODO PARTE CENTRAL DEL CUADRADO */
-        /* MiddleSquare middleSquare = new MiddleSquare();
-         List<Double> numbers = middleSquare.getNumbers(123, 4, 5);
-        numbers.forEach(System.out::println); */
-
-        /* METODO CONGRUENCIAL MULTIPLICATIVO */
-        /* MultCongruential multCongruential = new MultCongruential();
-        List<Double> numbers = multCongruential.getNumbers(1317, 5631, 547, 5);
-        numbers.forEach(System.out::println); */
-
-        /* METODO CONGRUENCIAL ADITIVO */
-        AdditCongruential additCongruential = new AdditCongruential();
-        List<Double> numbers = additCongruential.getNumbers(3, 5147, 3);
-        System.out.println("Números generados por el Metodo Congruencial Aditivo:\t");
-        numbers.forEach(System.out::println);
-
-        /* METODO LEHMER */
-        Lehmer lehmer = new Lehmer();
-        List<Double> numbersLehmer = lehmer.getNumbers(73, 35451, 8);
-        System.out.println("Numeros generados por el Metodo Lehmer:\t");
-        numbersLehmer.forEach(System.out::println);
-        
-        /*METODO CONGRUENCIAL MIXTO*/
-       MixedCongruential mc = new MixedCongruential(5, 1, 16, 7);
-       System.out.println("Números generados por el Método Congruencial Mixto:\t");
-       mc.generate(10).forEach(System.out::println);
 
     }
 }

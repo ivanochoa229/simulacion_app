b5bde8f3e355c6a46fbcaeb8fece99359d85c336
ivanochoa229 <ivanochoa229@gmail.com>
2025-04-22 19:04:58 -0300

    agregando estadistico

--- src/main/java/org/simulacion/test/FrequencyTest.java
+++ src/main/java/org/simulacion/test/FrequencyTest.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class FrequencyTest implements TestInterface {
     @Override
     public Boolean checkSample(List<Double> sample, List<Integer> conditions) {
+        /****
+         * Grados de Libertad sample.size()-1
+         *
+         * */
         return null;
     }
 }

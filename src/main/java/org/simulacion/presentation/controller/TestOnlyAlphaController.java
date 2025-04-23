797fa37fa40995914ad686585eb20af11249c0c5
ivanochoa229 <ivanochoa229@gmail.com>
2025-04-22 22:07:46 -0300

    se agregaron las interfaces para los nuevos test

---
+++ src/main/java/org/simulacion/presentation/controller/TestOnlyAlphaController.java
@@ -0,0 +1,31 @@
+package org.simulacion.presentation.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class TestOnlyAlphaController {
+
+
+    @FXML
+    private ComboBox<?> jComboAlpha;
+
+    @FXML
+    private Label labelTest;
+
+    @FXML
+    void doTest(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backSelectTest(ActionEvent event) {
+
+    }
+}
--- src/main/java/org/simulacion/utils/Path.java
+++ src/main/java/org/simulacion/utils/Path.java
@@ -7,4 +7,6 @@ public class Path {
     public static final String MIXED_CONGRUENTIAL_CONTROLLER = "/MixedCongruential.fxml";
     public static final String MULTIPLICATIVE_CONGRUENTIAL_CONTROLLER = "/MultiplicativeCongruential.fxml";
     public static final String LEHMER_CONTROLLER = "/Lehmer.fxml";
+    public static final String TEST_ONLY_ALPHA_CONTROLLER = "TestOnlyAlpha.fxml";
+    public static final String TEST_ALPHA_INTERVAL_CONTROLLER = "/TestAlphaAndInterval.fxml";
 }

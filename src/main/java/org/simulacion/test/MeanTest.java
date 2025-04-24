cc3b1338147594ba43788b5e3d6834d0f36c62af
lourdespalaciosillanes <75710327+lourdespalaciosillanes@users.noreply.github.com>
2025-04-23 22:58:28 -0300

    se agrego prueba de promedio

--- src/main/java/org/simulacion/test/MeanTest.java
+++ src/main/java/org/simulacion/test/MeanTest.java
@@ -2,10 +2,30 @@ package org.simulacion.test;
 
 import java.util.List;
 
+import org.simulacion.utils.Stadistics;
+
 public class MeanTest implements TestInterface {
 
     @Override
     public Boolean checkSample(List<Double> sample, List<Integer> conditions) {
-        return null;
+        int n = sample.size(); // Tamaño de la muestra
+        int alfaint = conditions.get(1); //Alfa en entero
+        double alfa = alfaint / 100.0;
+
+        // 1. Calcular el promedio aritmético X̄
+        double sum = 0;
+        for (double value : sample) {
+            sum += value;
+        }
+        double promedio = sum / n;
+
+        // 2. Calcular el estadístico Z₀
+        double z0 = (promedio - 0.5) * Math.sqrt(n) / Math.sqrt(1.0 / 12);
+
+        // 3. Obtener Zα usando la clase Stadistics
+        double zAlpha = Stadistics.zNormal(alfa);
+
+        // 4. Comparar |Z₀| < Zα
+        return Math.abs(z0) < zAlpha;
     }
 }
